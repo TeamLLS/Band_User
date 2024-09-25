9fff0fd1f359a3f7c5a4323a08bc651364263fc5
DESKTOP-H6SN6G2\shsms <shsmsdo3601@gmail.com>
2024-09-25 16:55:50 +0900

    rename project

--- src/test/java/com/example/band_authentication/BandAuthenticationApplicationTests.java
+++ src/test/java/com/example/band_authentication/BandAuthenticationApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.band_authentication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BandAuthenticationApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
