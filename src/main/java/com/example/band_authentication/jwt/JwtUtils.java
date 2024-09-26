48f1a21500acbf9f133c9132cb6fe0ffb669034c
SeoJue <shsmsdo3601@gmail.com>
2024-09-26 14:27:54 +0900

    edit config

--- src/main/java/com/example/band_authentication/jwt/JwtUtils.java
+++ src/main/java/com/example/band_authentication/jwt/JwtUtils.java
@@ -3,11 +3,9 @@ package com.example.band_authentication.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.HashMap;
